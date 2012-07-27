3b21a08684b5b04ad579d3eb8a902fa7b229c4a3
danielconnor <danielconnr@gmail.com>
2012-07-27 21:13:19 +0100

    improve comments

--- main.js
+++ main.js
@@ -4,38 +4,10 @@ var websocket = require("websocket");
 var http = require("http");
 var Inspector = require("./lib/Inspector.js");
 
-function getPages(cb) {
 
-  http.get({
-    hostname: "localhost",
-    port: 9222,
-    path: "/json"
-  }, function(response) {
-    var jsonString = "";
-
-    response.on("data", function(chunk) {
-      jsonString += chunk.toString();
-    });
-
-    response.on("end", function() {
-      var jsonData;
-
-      try {
-        jsonData = JSON.parse(jsonString);
-      }
-      catch(e) {
-        return cb(e);
-      }
-
-      cb(null, jsonData);
-    });
-  }).on("error", cb);
-}
-
-
-getPages(function(err, pages) {
+Inspector.getPages("localhost", 9222, function(err, pages) {
   var debuggerUrl = pages[0].webSocketDebuggerUrl;
-
+  console.log(pages);
   var i = new Inspector(debuggerUrl);
 
   console.log(debuggerUrl);
