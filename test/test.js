e69e7ce8281a201aa73905d12abcdb08c85e774b
danielconnor <danielconnr@gmail.com>
2012-08-30 08:12:01 +0100

    add caching models for app data

--- test/test.js
+++ test/test.js
@@ -8,57 +8,23 @@ var InspectorProxy = devtools.InspectorProxy;
 
 Inspector.getActivePages("localhost", 9222, function(err, pages) {
   if(pages.length) {
-    var debuggerUrl = pages[0].webSocketDebuggerUrl,
-      i = new InspectorProxy();
+
+    var debuggerUrl;
+    for(var p = 0; p < pages.length; p++) {
+      if(/localhost:8888/.test(pages[p].url)) {
+        debuggerUrl = pages[p].webSocketDebuggerUrl;
+      }
+    }
+
+    var i = new InspectorProxy();
 
     i.on("loaded", function() {
 
     });
 
-    i.connect(debuggerUrl, 9333);
+    if(debuggerUrl) {
+      i.connect(debuggerUrl, 9333);
+      console.log("listening on port: 9333");
+    }
   }
 });
-
-// Inspector.getActivePages("localhost", 9222, function(err, pages) {
-//   if(pages.length) {
-
-//     var debuggerUrl = pages[0].webSocketDebuggerUrl,
-//       i = new Inspector(debuggerUrl),
-//       dbg = i.components.Debugger,
-//       c = i.components.Console,
-//       files = [];
-
-//     var server = http.createServer(function(request, response) {});
-//     server.listen(8080, function() {});
-
-//     wsServer = new WebSocketServer({
-//         httpServer: server,
-//         autoAcceptConnections: false
-//     });
-
-//     dbg.on("scriptparsed", function(file) {
-//       // console.log(file.url);
-
-//       if(file.url === "http://localhost:8888/app/test.js") {
-//         file.setSource("function doStuff(e) { console.log('hello world');}", function(err, res) {
-//           console.log(res.result);
-//         });
-//       }
-//     });
-
-
-//     wsServer.on('request', function(request) {
-//       i.socket.on("connect",function(connection) {
-//         var c = request.accept(null);
-//         c.socket.pipe(connection.socket);
-//         connection.socket.pipe(c.socket);
-
-//         i.init(connection);
-//       });
-
-//       i.socket.connect(i.wsUrl);
-//     });
-
-
-//   }
-// });
